24f12994eb03a6ed3dc8fdfb0226e190e453868c
richardli166 <water.richard.li@gmail.com>
2023-11-15 09:14:38 -0500

    aaaaaaaa

--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -4,196 +4,189 @@ const chinesefoodmenu = [
     price: "$8.99",
     pieces: "1 serving",
     type: "Rice",
-    imageSrc: "https://example.com/egg-fried-rice.jpg"
+    imageSrc: "../Egg-Fried-Rice.jpg"
   },
   {
     name: "Ma Po Tofu",
     price: "$9.99",
     pieces: "1 serving",
     type: "Tofu",
-    imageSrc: "https://example.com/ma-po-tofu.jpg"
+    imageSrc: "../Ma-Po-Tofu.jpg"
   },
   {
     name: "Mongolian Beef",
     price: "$13.99",
     pieces: "1 serving",
     type: "Beef",
-    imageSrc: "https://example.com/mongolian-beef.jpg"
+    imageSrc: "../Mongolian-Beef.jpg"
   },
   {
     name: "Spring Rolls",
     price: "$5.99",
     pieces: "4 pieces",
     type: "Appetizer",
-    imageSrc: "https://example.com/spring-rolls.jpg"
+    imageSrc: "../Sping-Rolls.jpg"
   },
   {
     name: "Hot and Sour Soup",
     price: "$4.99",
     pieces: "1 serving",
     type: "Soup",
-    imageSrc: "https://example.com/hot-and-sour-soup.jpg"
+    imageSrc: "../Hot-And-Sour-Soup.jpg"
   },
   {
     name: "Szechuan Shrimp",
     price: "$12.99",
     pieces: "8 pieces",
     type: "Shrimp",
-    imageSrc: "https://example.com/szechuan-shrimp.jpg"
+    imageSrc: "../Szechuan-Shrimp.jpg"
   },
   {
     name: "Moo Goo Gai Pan",
     price: "$10.99",
     pieces: "1 serving",
     type: "Chicken",
-    imageSrc: "https://example.com/moo-goo-gai-pan.jpg"
-  },
-  {
-    name: "Broccoli Beef",
-    price: "$11.99",
-    pieces: "1 serving",
-    type: "Beef",
-    imageSrc: "https://example.com/broccoli-beef.jpg"
+    imageSrc: "../Moo-Goo-Gai-Pan.jpg"
   },
   {
     name: "Peking Duck",
     price: "$16.99",
     pieces: "1 serving",
     type: "Duck",
-    imageSrc: "https://example.com/peking-duck.jpg"
+    imageSrc: "../Peking-Duck.jpg"
   },
   {
     name: "Yangzhou Fried Rice",
     price: "$9.99",
     pieces: "1 serving",
     type: "Rice",
-    imageSrc: "https://example.com/yangzhou-fried-rice.jpg"
+    imageSrc: "../Yangzhou-Fried-Rice.jpg"
   },
   {
     name: "Shrimp Dumplings",
     price: "$8.99",
     pieces: "6 pieces",
     type: "Dumplings",
-    imageSrc: "https://example.com/shrimp-dumplings.jpg"
+    imageSrc: "../Shrimp-Dumplings.jpg"
   },
   {
     name: "Mapo Eggplant",
     price: "$9.99",
     pieces: "1 serving",
     type: "Eggplant",
-    imageSrc: "https://example.com/mapo-eggplant.jpg"
+    imageSrc: "../Mapo-Eggplant.jpg"
   },
   {
     name: "Honey Walnut Shrimp",
     price: "$12.99",
     pieces: "10 pieces",
     type: "Shrimp",
-    imageSrc: "https://example.com/honey-walnut-shrimp.jpg"
+    imageSrc: "../Honey-Walnut-Shrimp.jpg"
   },
   {
     name: "Pork Chow Mein",
     price: "$10.99",
     pieces: "1 serving",
     type: "Pork",
-    imageSrc: "https://example.com/pork-chow-mein.jpg"
+    imageSrc: "../Pork-Chow-Mein.jpg"
   },
   {
     name: "Steamed Sea Bass",
     price: "$15.99",
     pieces: "1 serving",
     type: "Fish",
-    imageSrc: "https://example.com/steamed-sea-bass.jpg"
+    imageSrc: "../Steam-Sea-Bass.jpg"
   },
   {
     name: "Stir-fried Noodles",
     price: "$8.99",
     pieces: "1 serving",
     type: "Noodles",
-    imageSrc: "https://natashaskitchen.com/wp-content/uploads/2018/08/Chicken-Stir-Fry-1-1.jpg"
+    imageSrc: "../Stir-Fry-Noodles.jpg"
   },
   {
     name: "Lemon Chicken",
     price: "$10.99",
     pieces: "10 pieces",
     type: "Chicken",
-    imageSrc: "https://thewoksoflife.com/wp-content/uploads/2023/10/chinese-lemon-chicken-16.jpg"
+    imageSrc: "../Lemon-Chicken.jpg"
   },
   {
     name: "Sesame Chicken",
     price: "$11.99",
     pieces: "12 pieces",
     type: "Chicken",
-    imageSrc: "https://twoplaidaprons.com/wp-content/uploads/2022/07/holding-up-a-piece-of-sesame-chicken-with-chopstick.jpg"
+    imageSrc: "../Sesame-Chicken.jpg"
   },
   {
     name: "Dim Sum Platter",
     price: "$13.99",
     pieces: "1 serving",
     type: "Appetizer",
-    imageSrc: "https://houseofmantou.com/storage/app/media/1House%20of%20Mantou/0000_DimsumPlatterSpecial.jpg"
+    imageSrc: "../Dimsum-Platter.jpg"
   },
   {
     name: "Scallion Pancake",
     price: "$5.99",
     pieces: "4 pieces",
     type: "Appetizer",
-    imageSrc: "https://mission-food.com/wp-content/uploads/2011/07/Chinese-Scallion-Pancakes-13.jpg"
+    imageSrc: "../Scallion-Pancakes.jpg"
   },
   {
     name: "Beef and Broccoli",
     price: "$11.99",
     pieces: "1 serving",
     type: "Beef",
-    imageSrc: "https://christieathome.com/wp-content/uploads/2020/09/Chinese-Beef-and-Broccoli-9.jpg"
+    imageSrc: "../Beef-and-Broccoli.jpg"
   },
   {
     name: "Crab Rangoon",
     price: "$6.99",
     pieces: "6 pieces",
     type: "Appetizer",
-    imageSrc: "https://www.punchbowl.com/gridfs/fs/4e8cd00c14f09215b00002b2-1317851151"
+    imageSrc: "../Crab-Rangoon.jpg"
   },
   {
     name: "Soy Sauce Noodles",
     price: "$8.99",
     pieces: "1 serving",
     type: "Noodles",
-    imageSrc: "https://www.loveandoliveoil.com/wp-content/uploads/2015/03/soy-sauce-noodles2-1-600x900.jpg"
+    imageSrc: "../Soy-Sauce-Noodles.jpg"
   },
   {
     name: "Lobster with Ginger and Scallions",
     price: "$19.99",
     pieces: "1 serving",
     type: "Seafood",
-    imageSrc: "https://thewoksoflife.com/wp-content/uploads/2014/12/ginger-scallion-lobster-14-1.jpg"
+    imageSrc: "../Lobster-With-Ginger-And-Scallions.jpg"
   },
   {
     name: "Shrimp with Lobster Sauce",
     price: "$12.99",
     pieces: "8 pieces",
     type: "Shrimp",
-    imageSrc: "https://trychinesegoodies.com/wp-content/uploads/2022/04/shrimp-with-lobster-sauce-1.jpg"
+    imageSrc: "../Shrimp-With-Lobster-Sauce.jpg"
   },
   {
     name: "Szechuan Beef",
     price: "$13.99",
     pieces: "1 serving",
     type: "Beef",
-    imageSrc: "https://omnivorescookbook.com/wp-content/uploads/2019/01/1812_Sichuan-Beef-Stir-Fry_800.jpg"
+    imageSrc: "../Sichuan-Beef.jpg"
   },
   {
     name: "Orange Chicken",
     price: "$11.99",
     pieces: "12 pieces",
     type: "Chicken",
-    imageSrc: "https://www.modernhoney.com/wp-content/uploads/2018/01/Chinese-Orange-Chicken-5-crop.jpg"
+    imageSrc: "../Orange-Chicken.jpg"
   },
   {
     name: "Fried Wontons",
     price: "$6.99",
     pieces: "6 pieces",
     type: "Appetizer",
-    imageSrc: "https://bakedbree.com/wp-content/uploads/2023/04/Feature4_Fried-Wontons-BB-scaled.jpg"
+    imageSrc: "../Fried-Wontons.jpg"
   }
 ];
 export {chinesefoodmenu};
