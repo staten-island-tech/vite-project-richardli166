4ebe04ae7b725510aae5e582cc3844fdaa8b10e3
richardli166 <water.richard.li@gmail.com>
2023-11-21 08:57:13 -0500

    pushfromschoolcomputer

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,27 +1,15 @@
 const DOMSelectors = {
-    form: document.querySelector("#form"),
-    openerName: document.querySelector(".name"),
-    discoveredBy: document.querySelector(".price"),
-    Card: document.querySelector("#carddisplay"),
+    all: document.querySelector("#all-button"),
+    Appetizer: document.querySelector("#appetizer-button"),
+    Chicken: document.querySelector("#chicken-button"),
+    Beef: document.querySelector("#beef-button"),
+    Pork: document.querySelector("#pork-button"),
+    Duck: document.querySelector("#duck-button"),
+    Seafood: document.querySelector("#seafood-button"),
+    Noodles: document.querySelector("#noodles-button"),
+    Vegetarian: document.querySelector("#vegetarian-button"),
+    Rice: document.querySelector("#rice-button"),
+    Soup: document.querySelector("#soup-button"),
+    allCards: document.querySelector(".allCards"),
 }
-
-function buttons(){
-    DOMSelectors.Card.insertAdjacentHTML("beforeend",
-    `<div class="inserts">
-        <h1 class="namecard">${DOMSelectors.name.value}</h1>
-        <h2>${DOMSelectors.price.value}</h2>
-    </div>`
-);
-}
-
-function clearfield(){
-    DOMSelectors.name.value = "";
-    DOMSelectors.price.value = "";
-    DOMSelectors.bag.value = "";
-}
-
-DOMSelectors.form.addEventListener("submit", function (event){
-    event.preventDefault();
-    input();
-    clearfield();
-    }); 
+export{DOMSelectors};
--- vite-project/js/menu.js
+++ vite-project/js/menu.js
@@ -1,192 +1,185 @@
 const chinesefoodmenu = [
- {
-    name: "Egg Fried Rice",
-    price: "$8.99",
-    pieces: "1 serving",
-    type: "Rice",
-    imageSrc: "../Egg-Fried-Rice.jpg"
-  },
   {
-    name: "Ma Po Tofu",
-    price: "$9.99",
-    pieces: "1 serving",
-    type: "Tofu",
-    imageSrc: "../Ma-Po-Tofu.jpg"
-  },
-  {
-    name: "Mongolian Beef",
-    price: "$13.99",
-    pieces: "1 serving",
-    type: "Beef",
-    imageSrc: "../Mongolian-Beef.jpg"
-  },
-  {
-    name: "Spring Rolls",
-    price: "$5.99",
-    pieces: "4 pieces",
-    type: "Appetizer",
-    imageSrc: "../Sping-Rolls.jpg"
-  },
-  {
-    name: "Hot and Sour Soup",
-    price: "$4.99",
-    pieces: "1 serving",
-    type: "Soup",
-    imageSrc: "../Hot-And-Sour-Soup.jpg"
-  },
-  {
-    name: "Szechuan Shrimp",
-    price: "$12.99",
-    pieces: "8 pieces",
-    type: "Shrimp",
-    imageSrc: "../Szechuan-Shrimp.jpg"
-  },
-  {
-    name: "Moo Goo Gai Pan",
-    price: "$10.99",
-    pieces: "1 serving",
-    type: "Chicken",
-    imageSrc: "../Moo-Goo-Gai-Pan.jpg"
-  },
-  {
-    name: "Peking Duck",
-    price: "$16.99",
-    pieces: "1 serving",
-    type: "Duck",
-    imageSrc: "../Peking-Duck.jpg"
-  },
-  {
-    name: "Yangzhou Fried Rice",
-    price: "$9.99",
-    pieces: "1 serving",
-    type: "Rice",
-    imageSrc: "../Yangzhou-Fried-Rice.jpg"
-  },
-  {
-    name: "Shrimp Dumplings",
-    price: "$8.99",
-    pieces: "6 pieces",
-    type: "Dumplings",
-    imageSrc: "../Shrimp-Dumplings.jpg"
-  },
-  {
-    name: "Mapo Eggplant",
-    price: "$9.99",
-    pieces: "1 serving",
-    type: "Veggie",
-    imageSrc: "../Mapo-Eggplant.jpg"
-  },
-  {
-    name: "Honey Walnut Shrimp",
-    price: "$12.99",
-    pieces: "10 pieces",
-    type: "Shrimp",
-    imageSrc: "../Honey-Walnut-Shrimp.jpg"
-  },
-  {
-    name: "Pork Chow Mein",
-    price: "$10.99",
-    pieces: "1 serving",
-    type: "Pork",
-    imageSrc: "../Pork-Chow-Mein.jpg"
-  },
-  {
-    name: "Steamed Sea Bass",
-    price: "$15.99",
-    pieces: "1 serving",
-    type: "Seafood",
-    imageSrc: "../Steam-Sea-Bass.jpg"
-  },
-  {
-    name: "Stir-fried Noodles",
-    price: "$8.99",
-    pieces: "1 serving",
-    type: "Noodles",
-    imageSrc: "../Stir-Fry-Noodles.jpg"
-  },
-  {
-    name: "Lemon Chicken",
-    price: "$10.99",
-    pieces: "10 pieces",
-    type: "Chicken",
-    imageSrc: "../Lemon-Chicken.jpg"
-  },
-  {
-    name: "Sesame Chicken",
-    price: "$11.99",
-    pieces: "12 pieces",
-    type: "Chicken",
-    imageSrc: "../Sesame-Chicken.jpg"
-  },
-  {
-    name: "Dim Sum Platter",
-    price: "$13.99",
-    pieces: "1 serving",
-    type: "Appetizer",
-    imageSrc: "../Dimsum-Platter.jpg"
-  },
-  {
-    name: "Scallion Pancake",
-    price: "$5.99",
-    pieces: "4 pieces",
-    type: "Appetizer",
-    imageSrc: "../Scallion-Pancakes.jpg"
-  },
-  {
-    name: "Beef and Broccoli",
-    price: "$11.99",
-    pieces: "1 serving",
-    type: "Beef",
-    imageSrc: "../Beef-and-Broccoli.jpg"
-  },
-  {
-    name: "Crab Rangoon",
-    price: "$6.99",
-    pieces: "6 pieces",
-    type: "Appetizer",
-    imageSrc: "../Crab-Rangoon.jpg"
-  },
-  {
-    name: "Soy Sauce Noodles",
-    price: "$8.99",
-    pieces: "1 serving",
-    type: "Noodles",
-    imageSrc: "../Soy-Sauce-Noodles.jpg"
-  },
-  {
-    name: "Lobster with Ginger and Scallions",
-    price: "$19.99",
-    pieces: "1 serving",
-    type: "Seafood",
-    imageSrc: "../Lobster-With-Ginger-And-Scallions.jpg"
-  },
-  {
-    name: "Shrimp with Lobster Sauce",
-    price: "$12.99",
-    pieces: "8 pieces",
-    type: "Shrimp",
-    imageSrc: "../Shrimp-With-Lobster-Sauce.jpg"
-  },
-  {
-    name: "Szechuan Beef",
-    price: "$13.99",
-    pieces: "1 serving",
-    type: "Beef",
-    imageSrc: "../Sichuan-Beef.jpg"
-  },
-  {
-    name: "Orange Chicken",
-    price: "$11.99",
-    pieces: "12 pieces",
-    type: "Chicken",
-    imageSrc: "../Orange-Chicken.jpg"
-  },
-  {
-    name: "Fried Wontons",
-    price: "$6.99",
-    pieces: "6 pieces",
-    type: "Appetizer",
-    imageSrc: "../Fried-Wontons.jpg"
-  }
-];
-export {chinesefoodmenu};
+     name: "Egg Fried Rice",
+     price: "$8.99",
+     pieces: "1 Serving",
+     type: "Rice",
+     imageSrc: "../Egg-Fried-Rice.jpg"
+   },
+   {
+     name: "Ma Po Tofu",
+     price: "$9.99",
+     pieces: "1 Serving",
+     type: "Vegetarian",
+     imageSrc: "../Ma-Po-Tofu.jpg"
+   },
+   {
+     name: "Mongolian Beef",
+     price: "$13.99",
+     pieces: "1 Serving",
+     type: "Beef",
+     imageSrc: "../Mongolian-Beef.jpg"
+   },
+   {
+     name: "Spring Rolls",
+     price: "$5.99",
+     pieces: "4 Pieces",
+     type: "Appetizer",
+     imageSrc: "../Spring-Roll.jpg"
+   },
+   {
+     name: "Hot and Sour Soup",
+     price: "$4.99",
+     pieces: "1 Serving",
+     type: "Soup",
+     imageSrc: "../Hot-And-Sour-Soup.jpg"
+   },
+   {
+     name: "Szechuan Shrimp",
+     price: "$12.99",
+     pieces: "8 Pieces",
+     type: "Seafood",
+     imageSrc: "../Szechuan-Shrimp.jpg"
+   },
+   {
+     name: "Moo Goo Gai Pan",
+     price: "$10.99",
+     pieces: "1 Serving",
+     type: "Chicken",
+     imageSrc: "../Moo-Goo-Gai-Pan.jpg"
+   },
+   {
+     name: "Peking Duck",
+     price: "$16.99",
+     pieces: "1 Serving",
+     type: "Duck",
+     imageSrc: "../Peking-Duck.jpg"
+   },
+   {
+     name: "Yangzhou Fried Rice",
+     price: "$9.99",
+     pieces: "1 Serving",
+     type: "Rice",
+     imageSrc: "../Yangzhou-Fried-Rice.jpg"
+   },
+   {
+     name: "Mapo Eggplant",
+     price: "$9.99",
+     pieces: "1 Serving",
+     type: "Vegetarian",
+     imageSrc: "../Mapo-Eggplant.jpg"
+   },
+   {
+     name: "Honey Walnut Shrimp",
+     price: "$12.99",
+     pieces: "10 Pieces",
+     type: "Seafood",
+     imageSrc: "../Honey-Walnut-Shrimp.jpg"
+   },
+   {
+     name: "Pork Chow Mein",
+     price: "$10.99",
+     pieces: "1 Serving",
+     type: "Pork",
+     imageSrc: "../Pork-Chow-Mein.jpg"
+   },
+   {
+     name: "Steamed Sea Bass",
+     price: "$15.99",
+     pieces: "1 Serving",
+     type: "Seafood",
+     imageSrc: "../Steamed-Sea-Bass.jpg"
+   },
+   {
+     name: "Stir-fried Noodles",
+     price: "$8.99",
+     pieces: "1 Serving",
+     type: "Noodles",
+     imageSrc: "../Stir-Fried-Noodles.jpg"
+   },
+   {
+     name: "Lemon Chicken",
+     price: "$10.99",
+     pieces: "10 Pieces",
+     type: "Chicken",
+     imageSrc: "../Lemon-Chicken.jpg"
+   },
+   {
+     name: "Sesame Chicken",
+     price: "$11.99",
+     pieces: "12 Pieces",
+     type: "Chicken",
+     imageSrc: "../Sesame-Chicken.jpg"
+   },
+   {
+     name: "Dim Sum Platter",
+     price: "$13.99",
+     pieces: "1 Serving",
+     type: "Appetizer",
+     imageSrc: "../Dim-Sum-Platter.jpg"
+   },
+   {
+     name: "Scallion Pancake",
+     price: "$5.99",
+     pieces: "4 Pieces",
+     type: "Appetizer",
+     imageSrc: "../Scallion-Pancakes.jpg"
+   },
+   {
+     name: "Beef and Broccoli",
+     price: "$11.99",
+     pieces: "1 Serving",
+     type: "Beef",
+     imageSrc: "../Beef-and-Broccoli.jpg"
+   },
+   {
+     name: "Crab Rangoon",
+     price: "$6.99",
+     pieces: "6 Pieces",
+     type: "Appetizer",
+     imageSrc: "../Crab-Rangoon.jpg"
+   },
+   {
+     name: "Soy Sauce Noodles",
+     price: "$8.99",
+     pieces: "1 Serving",
+     type: "Noodles",
+     imageSrc: "../Soy-Sauce-Noodles.jpg"
+   },
+   {
+     name: "Lobster with Ginger and Scallions",
+     price: "$19.99",
+     pieces: "1 Serving",
+     type: "Seafood",
+     imageSrc: "../Lobster-With-Ginger-And-Scallions.jpg"
+   },
+   {
+     name: "Shrimp with Lobster Sauce",
+     price: "$12.99",
+     pieces: "8 Pieces",
+     type: "Seafood",
+     imageSrc: "../Shrimp-with-Lobster-Sauce.jpg"
+   },
+   {
+     name: "Szechuan Beef",
+     price: "$13.99",
+     pieces: "1 Serving",
+     type: "Beef",
+     imageSrc: "../Szechuan-Beef.jpg"
+   },
+   {
+     name: "Orange Chicken",
+     price: "$11.99",
+     pieces: "12 Pieces",
+     type: "Chicken",
+     imageSrc: "../Orange-Chicken.jpg"
+   },
+   {
+     name: "Fried Wontons",
+     price: "$6.99",
+     pieces: "6 Pieces",
+     type: "Appetizer",
+     imageSrc: "../Fried-Wontons.jpg"
+   }
+ ];
+ export {chinesefoodmenu};
